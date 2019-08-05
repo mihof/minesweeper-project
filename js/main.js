23203d1e8f2f727eee9dd9e7ed0dfca8c9ab6df3
Michael Hofmann <michaelhofmann@Michaels-MacBook-Pro.local>
2019-08-04 23:13:55 -0700

    Added shuffle and 1D to 2D function (Still needs work)

--- js/main.js
+++ js/main.js
@@ -2,28 +2,48 @@
 
 
 // State variables
-const board, winner, reset;
+let board, winner, reset;
 
 //Functions
+init();
 function init() {
-    board = [
-        [0, 1, "X", "X", 1, 1, 1, 1],
-        [0, 1, 1, 1, 1, 1, "X", 1],
-        [0, 0, 0, 0, 0, 1, 1, 1],
-        [0, 0, 0, 1, "X", 1, 1, 0],
-        [0, 0, 0, 1, "X", "X", 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, "X", 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, "X", 1, 0, 0],
-    ]
+    board = new Array(64).fill("X", 0, 10, null);
     winner = null;
-    render();
+    console.log(board);
+    console.log(shuffle());
+    //render();
 }
 
+function shuffle() {
+    //Shuffle the board (Durstenfeld Shuffle)
+    for (let i = board.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [board[i], board[j]] = [board[j], board[i]];
+    }
+}
+
+function dimensionArr() {
+    //Turns board into a 2D array
+    board = [], i, k;
+
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % SubArr === 0) {
+            k++;
+            matrix[k] = [];
+        }
+
+        board[k].push(list[i]);
+    }
+
+    return board;
+}
+
+
 function render() {
-    board.forEach(function(strip, indx) {
-        strip.forEach(function(tile, idx) {
-            
+    board.forEach(function(lat, indx) {
+        strip.forEach(function(long, idx) {
+            let tile = document.getElementById(`tile${idx}`);
+            //Connects the array to the buttons
         })
         
     });
