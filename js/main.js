1f41e0aacfc4cac816fda033ad837fefea4ce813
Michael Hofmann <michaelhofmann@Michaels-MacBook-Pro-2.local>
2019-08-05 14:41:06 -0700

    1D array to 2D array working, += 1 to area surrounding bomb commented out

--- js/main.js
+++ js/main.js
@@ -7,41 +7,43 @@ let board, winner, reset;
 //Functions
 init();
 function init() {
-    board = new Array(64).fill("X", 0, 10, null);
+    board = new Array(64).fill("X", 0, 12, 0);
     winner = null;
     shuffle(board);
-    console.log(board);
+    let newBoard = [];
+    while (board.length) newBoard.push(board.splice(0, 8));
+    /*newBoard.forEach(function(strip, indx) {
+        strip.forEach(function(tile, idx) {
+                if (tile === "X") {
+                    strip[idx - 1] += 1;
+                    strip[indx - 1][idx] += 1;
+                    strip[indx - 1][idx - 1] += 1;
+                    strip[indx - 1][idx + 1] += 1;
+                    strip[idx + 1] += 1;
+                    strip[indx + 1][idx] += 1;
+                    strip[indx + 1][idx - 1] += 1;
+                    strip[indx + 1][idx + 1] += 1;
+
+                };
+        });
+    });*/
+    console.log(newBoard);
     //render();
 }
 
 function shuffle(board) {
     //Shuffle the board (Durstenfeld Shuffle)
     for (let i = board.length -1; i > 0; i--) {
+        //Randomly selecting values from the array, but not more than once per value
         const j = Math.floor(Math.random() * (i + 1));
+        //Placing values into the array (i is the index, j is the value)
         [board[i], board[j]] = [board[j], board[i]];
     }
 }
 
 
-function dimensionArr(list, SubArr) {
-    //Turns board into a 2D array
-    board = [], i, k;
-
-    for (i = 0, k = -1; i < list.length; i++) {
-        if (i % SubArr === 0) {
-            k++;
-            matrix[k] = [];
-        }
-
-        board[k].push(list[i]);
-    }
-
-    return board;
-}
-
-
 function render() {
-    board.forEach(function(lat, indx) {
+    newBoard.forEach(function(lat, indx) {
         strip.forEach(function(long, idx) {
             let tile = document.getElementById(`tile${idx}`);
             //Connects the array to the buttons
