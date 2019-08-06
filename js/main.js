410e316be8da5812be1acd647ed879b086eb4e98
Michael Hofmann <michaelhofmann@Michaels-MacBook-Pro-2.local>
2019-08-05 22:52:37 -0700

    Complete overhaual, buttons now work

--- js/main.js
+++ js/main.js
@@ -1,54 +1,110 @@
 // constants
-
+const NUM_MINES = 12;
 
 // State variables
-let board, winner, reset;
+let board, winner, reset, gameOver;
+
+// Event listeners
+for (let i = 0; i < 64; i++) {
+    document.getElementById(`tile${i}`).addEventListener('click', handleClick(i));
+    document.getElementById(`tile${i}`).addEventListener('contextmenu', handleClick(i));
+}
+
+// Square class definition
+class Square {
+    constructor (hidden, flag, value) {
+        this.hidden = hidden;
+        this.flag = flag;
+        this.value = value;
+    }
+}
 
 //Functions
 init();
 function init() {
-    board = new Array(64).fill("X", 0, 12, 0);
-    winner = null;
-    shuffle(board);
-    let newBoard = [];
-    while (board.length) newBoard.push(board.splice(0, 8));
-    /*newBoard.forEach(function(strip, indx) {
-        strip.forEach(function(tile, idx) {
-                if (tile === "X") {
-                    strip[idx - 1] += 1;
-                    strip[indx - 1][idx] += 1;
-                    strip[indx - 1][idx - 1] += 1;
-                    strip[indx - 1][idx + 1] += 1;
-                    strip[idx + 1] += 1;
-                    strip[indx + 1][idx] += 1;
-                    strip[indx + 1][idx - 1] += 1;
-                    strip[indx + 1][idx + 1] += 1;
+    board = [
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+        [new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0), new Square(true, false, 0)],
+    ];
+    gameOver = false;
+    layMines();
+    setNumbers();
 
-                };
-        });
-    });*/
-    console.log(newBoard);
     //render();
 }
 
-function shuffle(board) {
-    //Shuffle the board (Durstenfeld Shuffle)
-    for (let i = board.length -1; i > 0; i--) {
-        //Randomly selecting values from the array, but not more than once per value
-        const j = Math.floor(Math.random() * (i + 1));
-        //Placing values into the array (i is the index, j is the value)
-        [board[i], board[j]] = [board[j], board[i]];
+function layMines() {
+    let numLaid = 0;
+    //Shuffle the row (Durstenfeld Shuffle)
+    while (numLaid < NUM_MINES) {
+        for (let i = 0; i < board.length - 1; i++) {
+            for (let j = 0; j < board[i].length - 1; j++) {
+                const random = Math.floor(Math.random() * 2);
+                if (random) {
+                    board[i][j].value = 'X';
+                    numLaid++;
+                }
+            }
+        }
     }
 }
 
+function setNumbers() {
+    for (let i = 0; i < board.length - 1; i++) {
+        for (let j = 0; j < board[i].length - 1; j++) {
+            if (board[i][j] === 'X') {
+                for (let k = -1; k < 2; k++) {
+                    for (let l = -1; l < 2; l++) {
+                        let tile = !(l == 0 && k == 0) && (i + k >= 0 && i + k < 8 && j + l >= 0 && j + l < 8);
+                        if (tile && board[i + k][j + l] !== 'X') {
+                            board[i + k][j + l].value++;
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
+
+function win() {
+    
+}
+
+function handleClick(index) {
+    return (event) => {
+        event.preventDefault();
+        if (gameOver) {
+            return;
+        }
+        if (event.button == 0) {
+            const row = Math.floor(index / 8);
+            const column = index % 8;
+            if (board[row][column].hidden) {
+                board[row][column].hidden = !board[row][column].hidden;
+            }
+            if (board[row][column].value === 'X') {
+                    gameOver = true;
+                    alert('Game Over');
+            }
+        } else if (event.button == 2) {
+            board[row][column].flag = !board[row][column].flag;
+        }
+    }
+};
 
 function render() {
-    newBoard.forEach(function(lat, indx) {
-        strip.forEach(function(long, idx) {
-            let tile = document.getElementById(`tile${idx}`);
+    Board.forEach(function (lat, i) {
+        board.forEach(function (long, j) {
+            let tile = document.getElementById(`tile${j}`);
             //Connects the array to the buttons
         })
-        
+
     });
 
 }
